2b6b41ff86f3078e5c1cb118df5ac927075d7eb1
alexsoble <asoble@gmail.com>
2013-07-04 11:44:42 -0500

    Fixed bug in Line model's formatting_time method

--- app/models/line.rb
+++ app/models/line.rb
@@ -3,22 +3,25 @@ class Line < ActiveRecord::Base
   belongs_to :interpretation
 
   def formatted_time
+
     time = self.time
+
     if time <= 9
       formatted_time = "00:0" + time.to_s
-    elsif 9 < time <= 59
+    elsif 9 < time && time <= 59
       formatted_time = "00:" + time.to_s
+    elsif 60 <= time && time < 540
+      if time%60 <= 9
+       formatted_time = "0" + ((time)/60).to_s + ":0" + ((time)%60).to_s
+      elsif 9 < time%60 <= 59 
+       formatted_time = "0" + ((time)/60).to_s + ":" + ((time)%60).to_s 
+     end
+    elsif 540 <= time && time < 3600 
+      if time%60 <= 9 then formatted_time = (time)/60 + ":0" + (time)%60 end
+      if 9 < time%60 && time%60 <= 59 then formatted_time = (time)/60 + ":" + (time)%60 end
     end
-    
-    # if 60 <= time < 540
-    #   if time%60 <= 9 then formatted_time = "0" + (time)/60 + ":0" + (time)%60 end
-    #   if 9 < time%60 <= 59 then formatted_time = "0" + (time)/60 + ":" + (time)%60 end
-    # end
-    # if 540 <= time < 3600 
-    #   if time%60 <= 9 then formatted_time = (time)/60 + ":0" + (time)%60 end
-    #   if 9 < time%60 <= 59 then formatted_time = (time)/60 + ":" + (time)%60 end
-    # end
     return formatted_time
+
   end
 
 end
