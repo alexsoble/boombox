d8d905115f49e998fe82e6184d9bcc7c6579d279
alexsoble <asoble@gmail.com>
2013-07-04 13:12:20 -0500

    'Show videos in...' now gets a list of video lang1s that actually have translations from the ApplicationController (new helper method)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
+  def translated_video_languages
+    arr = []
+    Video.order("created_at ASC").each do |v|
+      arr << v.lang1
+    end
+    arr
+  end 
+  helper_method :translated_video_languages
+
   private
 
   def current_user
