9c0137a5beff70fc45a7d122cbf17ace1f65e175
alexsoble <asoble@gmail.com>
2013-07-30 18:31:05 -0500

    Moved some stuff around so it would play nice with the new quiz routes & files.

--- app/controllers/interpretations_controller.rb
+++ app/controllers/interpretations_controller.rb
@@ -73,7 +73,7 @@ class InterpretationsController < ApplicationController
       @clip = false
     end
 
-@    @lines_have_lang1 = false
+    @lines_have_lang1 = false
     line_length = []
     @lines.all.each do |l|
       if l.lang1.present? then @lines_have_lang1 = true end
