18488710006c28fa83287ba35e4d7b57ea0a4348
alexsoble <asoble@gmail.com>
2013-08-06 15:17:57 -0500

    Uncommenting the migrations. They weren't actually getting through to the herok db anyway. Going to do a heroku pg reset and then restore from backup. In the future will do all db-related changes in a staging environment first.

--- db/migrate/20130730213212_rename_type_to_quiz_type.rb
+++ db/migrate/20130730213212_rename_type_to_quiz_type.rb
@@ -1,6 +1,6 @@
 class RenameTypeToQuizType < ActiveRecord::Migration
   def change
-    # remove_column :quizzes, :type
-    # add_column :quizzes, :quiz_type, :string
+    remove_column :quizzes, :type
+    add_column :quizzes, :quiz_type, :string
   end
 end
