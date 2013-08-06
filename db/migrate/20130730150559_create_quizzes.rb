df26fd4b4d0f74b44465a1f01fc51826be70cdb3
alexsoble <asoble@gmail.com>
2013-08-06 13:28:10 -0500

    Commenting out the 'create quizzes' migration in the hopes that it will get Heroku to play with me nicely.

--- db/migrate/20130730150559_create_quizzes.rb
+++ db/migrate/20130730150559_create_quizzes.rb
@@ -1,10 +1,10 @@
 class CreateQuizzes < ActiveRecord::Migration
   def change
-    create_table :quizzes do |t|
-      t.integer :interpretation_id
-      t.string :type
+    # create_table :quizzes do |t|
+    #   t.integer :interpretation_id
+    #   t.string :type
 
-      t.timestamps
+    #   t.timestamps
     end
   end
 end
