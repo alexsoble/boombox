d8db95dede77e5f83633b3836bfafbc44c7bb7d0
alexsoble <asoble@gmail.com>
2013-08-06 13:37:53 -0500

    Double ooof. Commenting out a few more tables. This sucks.

--- db/migrate/20130730150754_create_words.rb
+++ db/migrate/20130730150754_create_words.rb
@@ -1,10 +1,10 @@
 class CreateWords < ActiveRecord::Migration
   def change
-    create_table :words do |t|
-      t.integer :quiz_id
-      t.string :text
+    # create_table :words do |t|
+    #   t.integer :quiz_id
+    #   t.string :text
 
-      t.timestamps
-    end
+    #   t.timestamps
+    # end
   end
 end
--- db/migrate/20130803214253_create_votes.rb
+++ db/migrate/20130803214253_create_votes.rb
@@ -1,11 +1,11 @@
 class CreateVotes < ActiveRecord::Migration
   def change
-    create_table :votes do |t|
-      t.integer :direction
-      t.integer :interpretation_id
-      t.integer :user_id
+    # create_table :votes do |t|
+    #   t.integer :direction
+    #   t.integer :interpretation_id
+    #   t.integer :user_id
 
-      t.timestamps
-    end
+    #   t.timestamps
+    # end
   end
 end
