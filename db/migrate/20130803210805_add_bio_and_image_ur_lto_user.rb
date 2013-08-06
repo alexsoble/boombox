2d84a6ab56bc2e088bddcb988a66313a64049a3d
alexsoble <asoble@gmail.com>
2013-08-06 13:46:20 -0500

    Now uncommenting. Heroku is such a fickle strumpet.

--- db/migrate/20130803210805_add_bio_and_image_ur_lto_user.rb
+++ db/migrate/20130803210805_add_bio_and_image_ur_lto_user.rb
@@ -1,6 +1,6 @@
 class AddBioAndImageUrLtoUser < ActiveRecord::Migration
   def change
-    # add_column :users, :image_url, :string
-    # add_column :users, :bio, :text
+    add_column :users, :image_url, :string
+    add_column :users, :bio, :text
   end
 end
