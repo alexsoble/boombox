fae2f1db0bb7d886a7ba9899d66c97c3e7891cf8
alexsoble <asoble@gmail.com>
2013-08-09 10:38:03 -0500

    Sketching out clips. So that teachers can tweet out clips and discuss language/translation. Also, did a merge with the quizmode branch. Still figuring out git.

--- app/controllers/clips_controller.rb
+++ app/controllers/clips_controller.rb
@@ -2,6 +2,15 @@ class ClipsController < ApplicationController
 
   def create
 
+    @clip = Clip.create(params[:clip])
+    render :json => { :data => @clip }
+
   end
   
+  def show
+
+    render 'interpretations#show'
+
+  end 
+
 end
