d8bd53c525ddbfa7a09896276b1b7664f9cf3a7a
alexsoble <asoble@gmail.com>
2013-08-14 12:56:20 -0500

    Updating the seeds file to give poor Mika two more seconds.

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,4 +8,4 @@ School.create([{:name => 'University of Chicago' },
 Clip.destroy_all
 Clip.create([{:interpretation_id => 27, :start => 14, :duration => 12 }, 
   {:interpretation_id => 28, :start => 2, :duration => 15 },
-  {:interpretation_id => 119, :start => 23, :duration => 16}])
+  {:interpretation_id => 119, :start => 23, :duration => 18}])
