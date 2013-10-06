e1515abe410d74a66f663687e1b0d4700250200b
alexsoble <asoble@gmail.com>
2013-10-06 12:27:17 -0500

    Tag show pages

--- app/controllers/definitions_controller.rb
+++ app/controllers/definitions_controller.rb
@@ -8,9 +8,21 @@ class DefinitionsController < ApplicationController
   def show
 
     @definition = Definition.find_by_id(params[:id])
-    @word = Word.find_by_id(@definition.word_id)
     @definition_creator = User.find_by_id(@word.user_id)
     @definition_video = Video.find_by_id(@word.video_id)
+
+  end
+
+  def destroy
+
+    @definition = Definition.find_by_id(params[:id])
+    @word = Word.find_by_id(@definition.word_id)
+    @definition_video = Video.find_by_id(@word.video_id)
+    @definition.destroy
+    @definition.save
+    @word.destroy
+    @word.save
+    redirect_to video_url(@definition_video)
 
   end
 
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,3 +1,20 @@
 class Tag < ActiveRecord::Base
   attr_accessible :name, :type_artist, :type_difficulty, :type_lang, :type_style, :user_id, :video_id
+
+  def category
+
+    if self.type_artist == true
+      return "artist"
+    elsif self.type_difficulty == true
+      return "difficulty"
+    elsif self.type_lang == true
+      return "language"
+    elsif self.type_style == true
+      return "style"
+    else
+      return ""
+    end
+
+  end
+
 end
