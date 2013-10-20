3d932872aeb321b7ead9fd7fbc9c90e5a57aad07
alexsoble <asoble@gmail.com>
2013-10-20 12:44:46 -0500

    Bunch of little fixes; solid suggestions from Dad

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,85 +1,85 @@
 #encoding: utf-8
 
-Interpretation.all.each do |i|
-  if i.lines.present?
-    @transcript = Transcript.create({user_id: i.user_id, video_id: i.video_id, interpretation_id: i.id})
-    @transcript.save
-    @create_translation = false
-    i.lines.each do |l|
-      l.transcript_id = @transcript.id
-      if l.lang2.present?
-        @create_translation = true
-      end
-      l.save
-    end
-    if @create_translation == true
-      @translation = Translation.create({user_id: i.user_id, video_id: i.video_id, transcript_id: @transcript.id})
-      @translation.save
-      i.lines.each do |l|
-        if l.lang2.present?
-          TranslatedLine.create({line_id: l.id, translation_id: @translation.id, lang2: l.lang2})
-        end
-      end
-    end
-  end
-end
+# Interpretation.all.each do |i|
+#   if i.lines.present?
+#     @transcript = Transcript.create({user_id: i.user_id, video_id: i.video_id, interpretation_id: i.id})
+#     @transcript.save
+#     @create_translation = false
+#     i.lines.each do |l|
+#       l.transcript_id = @transcript.id
+#       if l.lang2.present?
+#         @create_translation = true
+#       end
+#       l.save
+#     end
+#     if @create_translation == true
+#       @translation = Translation.create({user_id: i.user_id, video_id: i.video_id, transcript_id: @transcript.id})
+#       @translation.save
+#       i.lines.each do |l|
+#         if l.lang2.present?
+#           TranslatedLine.create({line_id: l.id, translation_id: @translation.id, lang2: l.lang2})
+#         end
+#       end
+#     end
+#   end
+# end
 
-Language.create([
-  {:name => "English" },
-  {:name => "Dutch"},
-  {:name => "Afrikaans"},
-  {:name => "Chinese (Mandarin)"},
-  {:name => "Cantonese"},
-  {:name => "Japanese"},
-  {:name => "Korean"},
-  {:name => "Vietnamese"},
-  {:name => "Thai"}, 
-  {:name => "Burmese"},
-  {:name => "Tagalog"},
-  {:name => "Hmong"},
-  {:name => "Khmer"},
-  {:name => "Lao"},
-  {:name => "French"},
-  {:name => "Spanish"},
-  {:name => "Italian"},
-  {:name => "Catalan"},
-  {:name => "Romanian"},
-  {:name => "Albanian"},
-  {:name => "Russian"},
-  {:name => "Polish"},
-  {:name => "Czech"},
-  {:name => "German"},
-  {:name => "Greek"},
-  {:name => "Welsh"},
-  {:name => "Norwegian"},
-  {:name => "Swedish"},
-  {:name => "Danish"},
-  {:name => "Finnish"},
-  {:name => "Icelandic"},
-  {:name => "Hebrew"},
-  {:name => "Arabic"},
-  {:name => "Persian"},
-  {:name => "Pashto"},
-  {:name => "Turkish"},
-  {:name => "Hindi"},
-  {:name => "Bengali"},
-  {:name => "Gujarati"},
-  {:name => "Punjabi"},
-  {:name => "Sindhi"},
-  {:name => "Urdu"},
-  {:name => "Marathi"},
-  {:name => "Telugu"},
-  {:name => "Tamil"},
-  {:name => "Sinhala"},
-  {:name => "Latin"},
-  {:name => "Swahili"},
-  {:name => "Yoruba"},
-  {:name => "Xhosa"},
-  {:name => "Amharic"},
-  {:name => "Javanese"},
-  {:name => "Malay"},
-  {:name => "Kazakh"},
-  {:name => "Tajik"},
-  {:name => "Tibetan"},
-  {:name => "Uyghur"},
-  {:name => "Uzbek"} ])
+# Language.create([
+#   {:name => "English" },
+#   {:name => "Dutch"},
+#   {:name => "Afrikaans"},
+#   {:name => "Chinese (Mandarin)"},
+#   {:name => "Cantonese"},
+#   {:name => "Japanese"},
+#   {:name => "Korean"},
+#   {:name => "Vietnamese"},
+#   {:name => "Thai"}, 
+#   {:name => "Burmese"},
+#   {:name => "Tagalog"},
+#   {:name => "Hmong"},
+#   {:name => "Khmer"},
+#   {:name => "Lao"},
+#   {:name => "French"},
+#   {:name => "Spanish"},
+#   {:name => "Italian"},
+#   {:name => "Catalan"},
+#   {:name => "Romanian"},
+#   {:name => "Albanian"},
+#   {:name => "Russian"},
+#   {:name => "Polish"},
+#   {:name => "Czech"},
+#   {:name => "German"},
+#   {:name => "Greek"},
+#   {:name => "Welsh"},
+#   {:name => "Norwegian"},
+#   {:name => "Swedish"},
+#   {:name => "Danish"},
+#   {:name => "Finnish"},
+#   {:name => "Icelandic"},
+#   {:name => "Hebrew"},
+#   {:name => "Arabic"},
+#   {:name => "Persian"},
+#   {:name => "Pashto"},
+#   {:name => "Turkish"},
+#   {:name => "Hindi"},
+#   {:name => "Bengali"},
+#   {:name => "Gujarati"},
+#   {:name => "Punjabi"},
+#   {:name => "Sindhi"},
+#   {:name => "Urdu"},
+#   {:name => "Marathi"},
+#   {:name => "Telugu"},
+#   {:name => "Tamil"},
+#   {:name => "Sinhala"},
+#   {:name => "Latin"},
+#   {:name => "Swahili"},
+#   {:name => "Yoruba"},
+#   {:name => "Xhosa"},
+#   {:name => "Amharic"},
+#   {:name => "Javanese"},
+#   {:name => "Malay"},
+#   {:name => "Kazakh"},
+#   {:name => "Tajik"},
+#   {:name => "Tibetan"},
+#   {:name => "Uyghur"},
+#   {:name => "Uzbek"} ])
