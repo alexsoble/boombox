c9a771bffed2f60274f5cc47f27c84f4e4e323b2
alexsoble <asoble@gmail.com>
2013-10-29 15:44:19 -0500

    New editing tool suite

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,29 +1,5 @@
 #encoding: utf-8
 
-# Interpretation.all.each do |i|
-#   if i.lines.present?
-#     @transcript = Transcript.create({user_id: i.user_id, video_id: i.video_id, interpretation_id: i.id})
-#     @transcript.save
-#     @create_translation = false
-#     i.lines.each do |l|
-#       l.transcript_id = @transcript.id
-#       if l.lang2.present?
-#         @create_translation = true
-#       end
-#       l.save
-#     end
-#     if @create_translation == true
-#       @translation = Translation.create({user_id: i.user_id, video_id: i.video_id, transcript_id: @transcript.id})
-#       @translation.save
-#       i.lines.each do |l|
-#         if l.lang2.present?
-#           TranslatedLine.create({line_id: l.id, translation_id: @translation.id, lang2: l.lang2})
-#         end
-#       end
-#     end
-#   end
-# end
-
 # Language.create([
 #   {:name => "English" },
 #   {:name => "Dutch"},
